9a4ccf53b4a686e243e28f5d6c629f04379d0cc3
keeyong <keeyonghan@hotmail.com>
2018-01-23 15:46:59 -0800

    Create learn_dict.py

---
+++ learn_dict.py
@@ -0,0 +1,28 @@
+# two different types of dictionary usage
+# 
+# Let's use first name and last name as an example
+# 1st approach is to use "first name" and "last name" as separate keys. this approach is preferred
+# 2nd approach is to use first name value as key and last name as value
+
+# ---- 1st apprach
+name_1 = { 'firstname': 'keeyong', 'lastname': 'han' }
+name_2 = { 'firstname': 'dennis', 'lastname': 'yang' }
+
+names = []
+names.append(name_1)
+names.append(name_2)
+
+for name in names:
+    print("1st approach - first name:" + name["firstname"] + ", last name:" + name["lastname"])
+
+# ---- 2nd approach
+name_1 = { 'keeyong': 'han' }
+name_2 = { 'dennis': 'yang' }
+
+names = []
+names.append(name_1)
+names.append(name_2)
+
+for name in names:
+    for key, value in name.items():
+        print("2nd approach - first name:" + key + ", last name:" + value)
